ed7c31e4e2d813d69141f0b246272db0f5c0c331
Svetlana Andreevna <MS.Andreeevna@yandex.ru>
2023-09-08 20:04:36 +0300

    feat: implemented custom buttons in the slider and the slides themselves

--- src/components/promoslider/PromoSlider.jsx
+++ src/components/promoslider/PromoSlider.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-import { Navigation } from 'swiper/modules'
+import { Autoplay, Navigation, Pagination } from 'swiper/modules'
+
 import { Swiper, SwiperSlide } from 'swiper/react'
+
+import { SlArrowLeft, SlArrowRight } from 'react-icons/sl'
 
 // Import Swiper styles
 import 'swiper/css'
@@ -18,66 +21,77 @@ import BGSlide4 from './../../assets/images/slider/bg5.jpg'
 const PromoSlider = () => {
 	return (
 		<>
-			<div className='bg_container'>
-				<Swiper
-					navigation={true}
-					modules={[Navigation]}
-					spaceBetween={1}
-					slidesPerView={1}
-					onSlideChange={() => console.log('slide change')}
-					onSwiper={swiper => console.log(swiper)}
-				>
-					<SwiperSlide>
-						<div className={styles.promo_container}>
-							<img src={BGSlide1} alt='Slide 2' />
-							<div className={styles.promo}>
-								<h4 className={styles.promo__item_title}>
-									Коллекция аксессуаров
-								</h4>
-								<Link className={styles.promo__item_link} to='#'>
-									подробнее...
-								</Link>
-							</div>
+			<Swiper
+				navigation={{ nextEl: '.next_slide', prevEl: '.prev_slide' }}
+				loop={true}
+				autoplay={{
+					delay: 2500,
+					pauseOnMouseEnter: true,
+					disableOnInteraction: false,
+				}}
+				modules={[Autoplay, Pagination, Navigation]}
+				spaceBetween={1}
+				slidesPerView={1}
+				onSlideChange={() => console.log('slide change')}
+				onSwiper={swiper => console.log(swiper)}
+			>
+				<SwiperSlide>
+					<div className={styles.promo_container}>
+						<img src={BGSlide1} alt='Slide 1' />
+						<div className={styles.promo}>
+							<h4 className={styles.promo__item_title}>
+								Коллекция аксессуаров
+							</h4>
+							<Link className={styles.promo__item_link} to='#'>
+								подробнее...
+							</Link>
 						</div>
-					</SwiperSlide>
-					<SwiperSlide>
-						<div className={styles.promo_container}>
-							<img src={BGSlide2} alt='Slide 2' />
-							<div className={styles.promo}>
-								<h4 className={styles.promo__item_title}>Новинки</h4>
-								<Link className={styles.promo__item_link} to='#'>
-									подробнее...
-								</Link>
-							</div>
+					</div>
+				</SwiperSlide>
+				<SwiperSlide>
+					<div className={styles.promo_container}>
+						<img src={BGSlide2} alt='Slide 2' />
+						<div className={styles.promo}>
+							<h4 className={styles.promo__item_title}>Новинки</h4>
+							<Link className={styles.promo__item_link} to='#'>
+								подробнее...
+							</Link>
 						</div>
-					</SwiperSlide>
-					<SwiperSlide>
-						<div className={styles.promo_container}>
-							<img src={BGSlide3} alt='Slide 3' />
-							<div className={styles.promo}>
-								<h4 className={styles.promo__item_title}>Акции месяца</h4>
-								<Link className={styles.promo__item_link} to='#'>
-									подробнее...
-								</Link>
-							</div>
+					</div>
+				</SwiperSlide>
+				<SwiperSlide>
+					<div className={styles.promo_container}>
+						<img src={BGSlide3} alt='Slide 3' />
+						<div className={styles.promo}>
+							<h4 className={styles.promo__item_title}>Акции месяца</h4>
+							<Link className={styles.promo__item_link} to='#'>
+								подробнее...
+							</Link>
 						</div>
-					</SwiperSlide>
-					<SwiperSlide>
-						<div className={styles.promo_container}>
-							<img src={BGSlide4} alt='Slide 4' />
-							<div className={styles.promo}>
-								<h4 className={styles.promo__item_title}>
-									Раздвижные конструкции
-								</h4>
-								<Link className={styles.promo__item_link} to='#'>
-									подробнее...
-								</Link>
-							</div>
+					</div>
+				</SwiperSlide>
+				<SwiperSlide>
+					<div className={styles.promo_container}>
+						<img src={BGSlide4} alt='Slide 4' />
+						<div className={styles.promo}>
+							<h4 className={styles.promo__item_title}>
+								Раздвижные конструкции
+							</h4>
+							<Link className={styles.promo__item_link} to='#'>
+								подробнее...
+							</Link>
 						</div>
-					</SwiperSlide>
-					...
-				</Swiper>
-			</div>
+					</div>
+				</SwiperSlide>
+
+				<div className='next_slide'>
+					<SlArrowRight />
+				</div>
+				<div className='prev_slide'>
+					{' '}
+					<SlArrowLeft />
+				</div>
+			</Swiper>
 		</>
 	)
 }
