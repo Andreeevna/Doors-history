07e2173d3be1ac0ff01a02d27ccbd325880cac0f
Svetlana Andreevna <MS.Andreeevna@yandex.ru>
2023-09-13 18:53:25 +0300

    feat: implemented a footer without a map

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 import Logo from '../logo/Logo'
 import './../../assets/styles/index.css'
 import styles from './Footer.module.css'
+
+import Tg from './../../assets/images/icons/telegram-svgrepo-com.svg'
+import Vk from './../../assets/images/icons/vk-svgrepo-com.svg'
+import Wh from './../../assets/images/icons/whatsapp-svgrepo-com.svg'
 
 const Footer = () => {
 	return (
@@ -10,17 +15,38 @@ const Footer = () => {
 			<div className='bg_container'>
 				<div className={styles.footer_top}>
 					<div className={styles.footer_top__contact}>
-						<h2 className={styles.footer_top__title}></h2>
+						<h2 className={styles.footer_top__title}>Контакты:</h2>
+						<Link to='tel:89102688080' className={styles.footer_top__link}>
+							8-910-268-80-80
+						</Link>{' '}
+						<Link to='tel:89536153888' className={styles.footer_top__link}>
+							8-953-615-38-88
+						</Link>
 					</div>
 					<Logo />
-					<div className={styles.map}></div>
+					<div className={styles.map}>
+						<h2 className={styles.map__title}>Адрес</h2>
+					</div>
 				</div>
 				<div className={styles.footer_bottom}>
 					<h2 className={styles.footer_bottom__title}>Мы в соцсетях:</h2>
-					<ul className={styles.footer_bottom__list}>
-						<li></li>
-						<li></li>
-						<li></li>
+					<ul className={styles.social__list}>
+						<li className={styles.social__item}>
+							<Link to='/' className={styles.social__link}>
+								<img className={styles.social__img} src={Vk} />
+							</Link>
+						</li>
+
+						<li className={styles.social__item}>
+							<Link to='/' className={styles.social__link}>
+								<img className={styles.social__img} src={Wh} />
+							</Link>
+						</li>
+						<li className={styles.social__item}>
+							<Link to='/' className={styles.social__link}>
+								<img className={styles.social__img} src={Tg} />
+							</Link>
+						</li>
 					</ul>
 				</div>
 			</div>
