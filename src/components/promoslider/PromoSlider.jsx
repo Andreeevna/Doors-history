5e96762db2a67d770c3df72685be57eb5ea55d87
Svetlana Andreevna <MS.Andreeevna@yandex.ru>
2023-09-18 14:59:45 +0300

    refactor: changed the rendering of the slide

--- src/components/promoslider/PromoSlider.jsx
+++ src/components/promoslider/PromoSlider.jsx
@@ -12,11 +12,7 @@ import 'swiper/css'
 import 'swiper/css/navigation'
 import './../../assets/styles/index.css'
 import styles from './PromoSlider.module.css'
-
-import BGSlide1 from './../../assets/images/slider/bg1.jpg'
-import BGSlide2 from './../../assets/images/slider/bg2.jpg'
-import BGSlide3 from './../../assets/images/slider/bg4.jpg'
-import BGSlide4 from './../../assets/images/slider/bg5.jpg'
+import { dataSlider } from './dataSlider'
 
 const PromoSlider = () => {
 	return (
@@ -32,57 +28,22 @@ const PromoSlider = () => {
 				modules={[Autoplay, Pagination, Navigation]}
 				spaceBetween={1}
 				slidesPerView={1}
-				onSlideChange={() => console.log('slide change')}
+				onSlideChange={() => console.log('slide change1')}
 				onSwiper={swiper => console.log(swiper)}
 			>
-				<SwiperSlide>
-					<div className={styles.promo_container}>
-						<img src={BGSlide1} alt='Slide 1' />
-						<div className={styles.promo}>
-							<h4 className={styles.promo__item_title}>
-								Коллекция аксессуаров
-							</h4>
-							<Link className={styles.promo__item_link} to='#'>
-								подробнее...
-							</Link>
+				{dataSlider.map(item => (
+					<SwiperSlide key={item.id}>
+						<div className={styles.promo_container}>
+							<img src={item.imageUrl} alt={item.alt} />
+							<div className={styles.promo}>
+								<h4 className={styles.promo__item_title}>{item.title}</h4>
+								<Link className={styles.promo__item_link} to={item.linkTo}>
+									подробнее...
+								</Link>
+							</div>
 						</div>
-					</div>
-				</SwiperSlide>
-				<SwiperSlide>
-					<div className={styles.promo_container}>
-						<img src={BGSlide2} alt='Slide 2' />
-						<div className={styles.promo}>
-							<h4 className={styles.promo__item_title}>Новинки</h4>
-							<Link className={styles.promo__item_link} to='#'>
-								подробнее...
-							</Link>
-						</div>
-					</div>
-				</SwiperSlide>
-				<SwiperSlide>
-					<div className={styles.promo_container}>
-						<img src={BGSlide3} alt='Slide 3' />
-						<div className={styles.promo}>
-							<h4 className={styles.promo__item_title}>Акции месяца</h4>
-							<Link className={styles.promo__item_link} to='#'>
-								подробнее...
-							</Link>
-						</div>
-					</div>
-				</SwiperSlide>
-				<SwiperSlide>
-					<div className={styles.promo_container}>
-						<img src={BGSlide4} alt='Slide 4' />
-						<div className={styles.promo}>
-							<h4 className={styles.promo__item_title}>
-								Раздвижные конструкции
-							</h4>
-							<Link className={styles.promo__item_link} to='#'>
-								подробнее...
-							</Link>
-						</div>
-					</div>
-				</SwiperSlide>
+					</SwiperSlide>
+				))}
 
 				<div className='next_slide'>
 					<SlArrowRight />
---
+++ src/components/promoslider/dataSlider.js
@@ -0,0 +1,30 @@
+export const dataSlider = [
+	{
+		id: 1,
+		imageUrl: '/images/slider/bg1.jpg',
+		alt: 'Slide 1',
+		title: 'Коллекция аксессуаров',
+		linkTo: '/',
+	},
+	{
+		id: 2,
+		imageUrl: '/images/slider/bg2.jpg',
+		alt: 'Slide 2',
+		title: 'Новинки',
+		linkTo: '/',
+	},
+	{
+		id: 3,
+		imageUrl: '/images/slider/bg4.jpg',
+		alt: 'Slide 3',
+		title: 'Акции месяца',
+		linkTo: '/',
+	},
+	{
+		id: 4,
+		imageUrl: '/images/slider/bg5.jpg',
+		alt: 'Slide 4',
+		title: 'Раздвижные конструкции',
+		linkTo: '/',
+	},
+]
