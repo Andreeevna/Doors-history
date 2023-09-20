171a64f4f01d39104674d4acd2fd62f9c62c2dfd
Svetlana Andreevna <MS.Andreeevna@yandex.ru>
2023-09-20 17:14:31 +0300

    feat: implemented for a more beautiful styling header on non-main pages of the site

---
+++ src/components/headerBottom/HeaderMain.jsx
@@ -0,0 +1,19 @@
+import cn from 'classnames'
+import React from 'react'
+
+import './../../assets/styles/index.css'
+import styles from './HeaderMain.module.css'
+
+import BG1 from './../../assets/images/backgrounds/header_top.jpg'
+
+const HeaderMain = () => {
+	return (
+		<section className={styles.header_main}>
+			<img src={BG1} alt='Интерьер' className={styles.header_main__img} />
+
+			<div className={cn('av_container', styles.header_main__info)}></div>
+		</section>
+	)
+}
+
+export default HeaderMain
--- src/screens/ProductPage/ProductPage.jsx
+++ src/screens/ProductPage/ProductPage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import HeaderMain from '../../components/headerBottom/HeaderMain'
 import Layout from '../../components/layout/Layout'
 import Product from '../../components/products/Product'
 
 const ProductPage = () => {
 	return (
 		<Layout>
+			<HeaderMain />
 			<Product />
 		</Layout>
 	)
