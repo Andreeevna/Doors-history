cfb023bff51d7229b1f3a6c05002729ae4618e1f
Svetlana Andreevna <MS.Andreeevna@yandex.ru>
2023-09-22 15:25:30 +0300

    feat: implemented a slice for products

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { apiSlice } from './api/apiSlice'
+import productsSlice from './products/productsSlice'
 
 export const store = configureStore({
-	reducer: {},
+	reducer: {
+		products: productsSlice,
+	},
 
 	middleware: getMiddleware => getMiddleware().concat(apiSlice.middleware),
 	devTools: true,
