1969ffce5792b6b72c8cbf6fbdda3919eb382f96
Svetlana Andreevna <MS.Andreeevna@yandex.ru>
2023-09-25 19:58:42 +0300

    feat: added the correct display of goods and implemented switching of the main product

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -11,26 +11,27 @@ const Products = () => {
 	return (
 		<section className={styles.products}>
 			<div className={cn('bg_container', styles.products_container)}>
-				{items.map(item => {
-					return (
-						<div className={styles.products_item} key={item.id}>
-							<Link
-								to={`/products/${item.id}`}
-								className={styles.products_item__img}
-							>
-								<img src={item.image} alt='Межкомнатная дверь' />
-							</Link>
-							<div className={styles.products_item__info}>
-								<div className={styles.products_item__text}>
-									<h2 className={styles.products_item__title}>{item.name}</h2>
-									<p className={styles.products_item__subtitle}>
-										{item.coverage}
-									</p>
+				{items &&
+					items.map(item => {
+						return (
+							<div className={styles.products_item} key={item.id}>
+								<Link
+									to={`/products/${item.id}`}
+									className={styles.products_item__img}
+								>
+									<img src={item.images[0]} alt='Межкомнатная дверь' />
+								</Link>
+								<div className={styles.products_item__info}>
+									<div className={styles.products_item__text}>
+										<h2 className={styles.products_item__title}>{item.name}</h2>
+										<p className={styles.products_item__subtitle}>
+											{item.coverage}
+										</p>
+									</div>
 								</div>
 							</div>
-						</div>
-					)
-				})}
+						)
+					})}
 			</div>
 		</section>
 	)
