eec3f5bd44715d3954965264b65b6df1c3c27045
Svetlana Andreevna <MS.Andreeevna@yandex.ru>
2023-09-27 13:39:06 +0300

    feat: created a markup for bread crumbs

--- src/components/breadcrumbs/Breadcrumbs.jsx
+++ src/components/breadcrumbs/Breadcrumbs.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
+import { useLocation } from 'react-router-dom'
+
+import styles from './Breadcrumbs.module.css'
+import Crumb from './Crumb'
 
 const Breadcrumbs = () => {
-	return <div>Breadcrumbs</div>
+	// state: {id, path, title, url}
+	const { state } = useLocation()
+	return (
+		<nav className={styles.breadcrumbs}>
+			{state.map(crumb => (
+				<Crumb {...crumb} key={crumb.url} />
+			))}
+		</nav>
+	)
 }
 
 export default Breadcrumbs
---
+++ src/components/breadcrumbs/Crumb.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Crumb = () => {
+	return <div>Crumb</div>
+}
+
+export default Crumb
