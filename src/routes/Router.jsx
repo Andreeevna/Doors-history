93052f7cb27644ceb6f30a859a11f1fbc0299de0
Svetlana Andreevna <MS.Andreeevna@yandex.ru>
2023-10-20 16:42:05 +0300

    refactor: rewritten paths

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,19 +1,23 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-import { routes } from './routes.data'
 
+import Product from '../components/products/Product'
+import Catalog from '../screens/Catalog/Catalog'
+import Home from '../screens/Home/Home'
+import LocationPage from '../screens/LocationPage/LocationPage'
+import ProductPage from '../screens/ProductPage/ProductPage'
+import StocksPage from '../screens/StocksPage/StocksPage'
 import './../assets/styles/index.css'
 
 const RouterApp = () => {
 	return (
 		<Routes>
-			{routes.map(route => (
-				<Route
-					key={route.path}
-					path={route.path}
-					element={<route.component />}
-				/>
-			))}
+			<Route path='/' element={<Home />} />
+			<Route path='products/:name?' element={<ProductPage />} />
+			<Route path='products/:name?/:id' element={<Product />} />
+			<Route path='/catalog' element={<Catalog />} />
+			<Route path='/stocks' element={<StocksPage />} />
+			<Route path='/location' element={<LocationPage />} />
 		</Routes>
 	)
 }
