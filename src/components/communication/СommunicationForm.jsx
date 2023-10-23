60122ca5e8bdc9ef3f05bc58ba554e9972659669
Svetlana Andreevna <MS.Andreeevna@yandex.ru>
2023-10-23 19:56:12 +0300

    feat: implemented the ability to send data to the mail

--- src/components/communication/СommunicationForm.jsx
+++ src/components/communication/СommunicationForm.jsx
@@ -1,13 +1,39 @@
-import React from 'react'
-
+import emailjs from '@emailjs/browser'
+import React, { useRef } from 'react'
 import styles from './СommunicationForm.module.css'
 
 const СommunicationForm = () => {
+	const form = useRef()
+
+	const sendEmail = e => {
+		e.preventDefault()
+
+		emailjs
+			.sendForm(
+				'service_j5yempc',
+				'template_7ntdx7c',
+				form.current,
+				'XxypEImd3uR6t2F3C'
+			)
+			.then(
+				result => {
+					console.log(result.text)
+				},
+				error => {
+					console.log(error.text)
+				}
+			)
+	}
+
 	return (
-		<form className={styles.form}>
-			<input type='text' placeholder='Ваше имя' />
-			<input type='number' placeholder='+7 (888) 888-88-88' />
-			<button type='submit' className={styles.form__submit_btn}>
+		<form ref={form} onSubmit={sendEmail} className={styles.form}>
+			<input type='text' placeholder='Ваше имя' name='user_name' />
+			<input
+				type='number'
+				placeholder='+7 (888) 888-88-88'
+				name='user_number'
+			/>
+			<button type='submit' value='Send' className={styles.form__submit_btn}>
 				Оставить заявку
 			</button>
 		</form>
